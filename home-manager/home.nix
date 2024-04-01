72b8bc46465dbbe616ceb4e99c108ceeb7608641
James Duff <jaduff@protonmail.com>
2024-04-01 19:45:22 +0800

    removed duplicated home-manager reference

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -108,9 +108,6 @@
     mediainfo
     kcalc
   ];
-  # Enable home-manager and git
-  programs.home-manager.enable = true;
-  programs.git.enable = true;
 
 
   # Nicely reload system units when changing configs
