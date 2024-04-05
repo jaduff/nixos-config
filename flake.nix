150f6ffaca5750756a54b1c4327d3d3c3ccb5267
James Duff <jaduff@protonmail.com>
2024-04-05 19:02:24 +0800

    added system

--- flake.nix
+++ flake.nix
@@ -11,27 +11,8 @@
     outputs = inputs:
         inputs.snowfall-lib.mkFlake {
             inherit inputs;
-	    modules = [ ./nixos/configuration.nix ];
+	    #modules = [ ./nixos/configuration.nix ];
             src = ./.;
 
-            # Configure Snowfall Lib, all of these settings are optional.
-            snowfall = {
-                # Tell Snowfall Lib to look in the `./nix/` directory for your
-                # Nix files.
-                #root = ./nix;
-
-                # Choose a namespace to use for your flake's packages, library,
-                # and overlays.
-                namespace = "carnifex-nix";
-
-                # Add flake metadata that can be processed by tools like Snowfall Frost.
-                meta = {
-                    # A slug to use in documentation when displaying things like file paths.
-                    name = "carnifex-nix";
-
-                    # A title to show for your flake, typically the name.
-                    title = "Carnifex Nix";
-                };
-            };
         };
 }
