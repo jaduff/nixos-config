c97f8f18ff5d490db05e550b3b52b059434f6297
James Duff <jaduff@protonmail.com>
2024-04-12 10:29:53 +0800

    enable plasma6

--- systems/x86_64-linux/carnifex-nix/plasma.nix
+++ systems/x86_64-linux/carnifex-nix/plasma.nix
@@ -6,15 +6,15 @@
 
 {
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  #services.xserver.enable = true;
 
-  # Enable the Plasma 5 Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  # Enable the Plasma 6 Desktop Environment.
+  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
   
 
   # Configure keymap in X11
-   services.xserver.xkb.layout = "us";
+   #services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
 }
