c7e59b052bc47ad188e0c8e64fa94c261b25915f
James Duff <jaduff@protonmail.com>
2024-04-12 20:28:05 +0800

    added window focus binds

--- homes/x86_64-linux/jaduff@carnifex-nix/hyprland.nix
+++ homes/x86_64-linux/jaduff@carnifex-nix/hyprland.nix
@@ -1,3 +1,37 @@
+
+
+
+
 {
   wayland.windowManager.hyprland.enable = true;
+ wayland.windowManager.hyprland.settings = {
+    "$mod" = "SUPER";
+    input.touchpad.natural_scroll = true;
+    bind =
+      [
+        "$mod, F, exec, firefox"
+        ", Print, exec, grimblast copy area"
+       # Window focus
+        "bind = SUPER, left, movefocus, l"
+        "bind = SUPER, right, movefocus, r"
+        "bind = SUPER, up, movefocus, u"
+        "bind = SUPER, down, movefocus, d"
+      ]
+      ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
+  };
 }
