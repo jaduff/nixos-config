4b7905bfd1cfe6508ae2bbbd468b5621b83ba6ab
James Duff <jaduff@protonmail.com>
2024-06-01 17:56:29 +0800

    updated

--- systems/x86_64-linux/carnifex-nix/default.nix
+++ systems/x86_64-linux/carnifex-nix/default.nix
@@ -23,6 +23,8 @@ nix.settings.trusted-substituters = ["https://ai.cachix.org"];
 nix.settings.trusted-public-keys = ["ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="];
 services.ntp.enable = true;
 
+virtualisation.libvirtd.enable = true;
+
 
   boot.initrd.systemd.enable = true;
   boot.supportedFilesystems = [ "ntfs" ];
